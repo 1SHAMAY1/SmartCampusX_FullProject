9b7f4c6866e1d54a2d22b2c8c24434ce47f241d4
1SHAMAY1 <139101765+1SHAMAY1@users.noreply.github.com>
2025-04-26 14:47:50 +0530

    Update axios.js

--- SmartCampusX_FullProject/SmartCampusX/frontend/src/api/axios.js
+++ SmartCampusX_FullProject/SmartCampusX/frontend/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:5000/api", // Change this to your deployed backend URL later
+  baseURL: "https://smartcampusx-fullproject-1.onrender.com", 
 });
